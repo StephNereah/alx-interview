21bc8a5c4c14b55df5640b9e231cf60d0cf13ad8
StephNereah <stphnadhiambo@gmail.com>
2024-10-16 20:31:39 +0300

    Technical mock interview

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,31 @@
 #!/usr/bin/python3
-"""Pascal"""
+"""Pascal Triangle Module."""
 
 
 def pascal_triangle(n):
-    """function to get the number of pascal triangle"""
+    """Generate Pascal's triangle up to the nth row.
+    
+    Args:
+        n (int): The number of rows in Pascal's triangle.
+    
+    Returns:
+        list: A list of lists representing Pascal's triangle.
+    """
     if n <= 0:
         return []
 
-    pascal_triangle = [0] * n
+    triangle = [0] * n
 
     for i in range(n):
-        new_row = [0] * (i+1)
+        new_row = [0] * (i + 1)
         new_row[0] = 1
-        new_row[len(new_row) - 1] = 1
+        new_row[-1] = 1
 
         for j in range(1, i):
-            if j > 0 and j < len(new_row):
-                a = pascal_triangle[i - 1][j]
-                b = pascal_triangle[i - 1][j - 1]
-                new_row[j] = a + b
+            a = triangle[i - 1][j]
+            b = triangle[i - 1][j - 1]
+            new_row[j] = a + b
 
-        pascal_triangle[i] = new_row
+        triangle[i] = new_row
 
-    return pascal_triangle
+    return triangle
